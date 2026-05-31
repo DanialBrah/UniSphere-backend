9d9eebfa0767b7c8c767fbcb943bcdbdd0038361
DanialBrah <danialx003@gmail.com>
2026-05-31 17:36:35 +0800

    Initialise Spring Boot backend with Spring Initializr

---
+++ src/main/java/com/unisphere/backend/UnisphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unisphere.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnisphereBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unisphere/backend/UnisphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisphere.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnisphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
