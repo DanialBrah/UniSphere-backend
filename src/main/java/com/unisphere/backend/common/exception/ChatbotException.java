74aff570bb34ede5499e47ad7da6d5546b78b670
DanialBrah <danialx003@gmail.com>
2026-06-12 02:19:19 +0800

    fix: added chatbot exception

--- src/main/java/com/unisphere/backend/common/exception/ChatbotException.java
+++ src/main/java/com/unisphere/backend/common/exception/ChatbotException.java
@@ -4,4 +4,8 @@ public class ChatbotException extends RuntimeException {
     public ChatbotException(String message) {
         super(message);
     }
+
+    public ChatbotException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
